6439fd4e35e1821c6b137c9a1d6c8285c5435cd7
Maksym Ochenashko <maxochenashko@gmail.com>
2024-02-02 14:10:23 +0200

    customize only macos

--- modules/cli/.native/src/main/scala/io/clickup/RuntimePlatform.scala
+++ modules/cli/.native/src/main/scala/io/clickup/RuntimePlatform.scala
@@ -1,7 +1,8 @@
 package io.clickup
 
 import cats.effect.unsafe.IORuntime
+import epollcat.unsafe.EpollRuntime
 
 object RuntimePlatform {
-  val default: IORuntime = IORuntime.global
+  val default: IORuntime = EpollRuntime.global
 }
--- project/NativeConfigOpts.scala
+++ project/NativeConfigOpts.scala
@@ -1,30 +1,33 @@
+import java.nio.file.Paths
+
 import scala.scalanative.build.{NativeConfig, Platform}
 
 // based on https://github.com/indoorvivants/sn-bindgen/blob/main/build.sbt
 object NativeConfigOpts {
   val llvmVersion = "17"
 
-  def customize(conf: NativeConfig): NativeConfig = {
-    val libraryName = "clang"
+  def customize(conf: NativeConfig): NativeConfig =
+    if (Platform.isMac) {
+      val libraryName            = "clang"
+      val (llvmInclude, llvmLib) = llvmFolder(conf.clang.toAbsolutePath)
 
-    val (llvmInclude, llvmLib) = llvmFolder(conf.clang.toAbsolutePath)
+      val arm64 =
+        if (sys.props.get("os.arch").contains("aarch64")) Seq("-arch", "arm64") else Nil
 
-    val arm64 =
-      if (sys.props.get("os.arch").contains("aarch64")) Seq("-arch", "arm64") else Nil
+      conf
+        .withLinkingOptions(
+          conf.linkingOptions ++
+            Seq("-l" + libraryName) ++
+            llvmLib.map("-L" + _) ++ arm64
+        )
+        .withCompileOptions(
+          conf.compileOptions ++ llvmInclude.map("-I" + _) ++ arm64
+        )
+    } else {
+      conf
+    }
 
-    conf
-      .withLinkingOptions(
-        conf.linkingOptions ++
-          Seq("-l" + libraryName) ++
-          llvmLib.map("-L" + _) ++ arm64
-      )
-      .withCompileOptions(
-        conf.compileOptions ++ llvmInclude.map("-I" + _) ++ arm64
-      )
-  }
-
-  def llvmFolder(clangPath: java.nio.file.Path): (List[String], List[String]) = {
-    import java.nio.file.Paths
+  private def llvmFolder(clangPath: java.nio.file.Path): (List[String], List[String]) =
     if (Platform.isMac) {
       val detected =
         sys.env
@@ -56,16 +59,7 @@ object NativeConfigOpts {
 
       includes -> lib
     } else {
-      // <llvm-path>/bin/clang
-      val realPath   = clangPath.toRealPath()
-      val binFolder  = realPath.getParent
-      val llvmFolder = binFolder.getParent
-
-      if (llvmFolder.toFile.exists())
-        List(llvmFolder.resolve("include").toString) -> List(llvmFolder.resolve("lib").toString)
-      else
-        Nil -> Nil
+      Nil -> Nil
     }
-  }
 
 }
